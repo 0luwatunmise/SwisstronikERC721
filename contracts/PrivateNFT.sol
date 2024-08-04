50628ad5b5d8f453144b7e2593458714187786cb
0luwatunmise <tunmisefasida@gmail.com>
2024-08-04 19:01:12 +0000

    Initial commit

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("youngprince","YPSZN")
+        ERC721("Swisstronik","$WI$$")
         Ownable(initialOwner)
     {}
 
